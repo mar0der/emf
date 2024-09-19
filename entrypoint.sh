ff31638cfd8f48e4ed8efc02436bd628dc2a9535
Petar Petkov <petar.b.petkov@gmail.com>
2024-09-19 23:22:34 +0400

    small changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-flask db upgrade
-python seed.py
-exec flask run --host=0.0.0.0
+set -e
+
+if [ "$1" = "python" ] && [ "$2" = "db_ops.py" ]; then
+    exec "$@"
+elif [ "$1" = "backup" ] || [ "$1" = "restore" ]; then
+    exec python db_ops.py "$@"
+else
+    flask db upgrade
+    python seed.py
+    exec python -m app
+fi
